4cae98ff5db92915c4577688525bbd125d2747aa
Agustin Gullino <agullino@itba.edu.ar>
2023-11-13 20:30:32 -0300

    Update helperRLS.py

--- helperRLS.py
+++ helperRLS.py
@@ -254,17 +254,19 @@ def plot_compare(resultsLMS, resultsRLS, P, S, xgen, soundgen):
     plt.title('SE vs n')
     plt.xlabel('n')
     plt.ylabel('Square error')
-    plt.semilogy(ran, J_lms, alpha=0.3)
-    plt.semilogy(ran, J_rls, alpha=0.3)
+    plt.semilogy(ran, J_lms, alpha=0.3, label='J LMS')
+    plt.semilogy(ran, J_rls, alpha=0.3, label='J RLS')
+    plt.legend()
 
     plt.figure(figsize=(10, 5))
     plt.grid()
     plt.title('Señal error vs t')
     plt.xlabel('t [s]')
     plt.ylabel('modulo del error [dB]')
-    plt.plot(ran, 20*np.log(np.abs(e_lms)), alpha=0.3)
-    plt.plot(ran, 20*np.log(np.abs(e_rls)), alpha=0.3)
+    plt.plot(ran, 20*np.log(np.abs(e_lms)), alpha=0.3, label='error LMS')
+    plt.plot(ran, 20*np.log(np.abs(e_rls)), alpha=0.3, label='error RLS')
     plt.ylim([-100,80])
+    plt.legend()
 
     plt.figure(figsize=(10, 5))
     plt.grid()
@@ -273,8 +275,8 @@ def plot_compare(resultsLMS, resultsRLS, P, S, xgen, soundgen):
     freqw_rls, wps_rls = sp.freqz(w_rls, [1], fs=48000, worN=10000)
     plt.title('Comparativa entre módulo de W(z) y P(z)/S(z)')
     plt.semilogx(freqs, 20*np.log10(np.abs(s)), label='P(z)/S(z)')
-    plt.semilogx(freqw_lms, 20*np.log10(np.abs(wps_lms)), label='W(z)', alpha = 0.3)
-    plt.semilogx(freqw_rls, 20*np.log10(np.abs(wps_rls)), label='W(z)', alpha = 0.3)
+    plt.semilogx(freqw_lms, 20*np.log10(np.abs(wps_lms)), label='$W_{LMS}(z)$', alpha = 0.3)
+    plt.semilogx(freqw_rls, 20*np.log10(np.abs(wps_rls)), label='$W_{RLS}(z)$', alpha = 0.3)
     plt.xlabel('f [Hz]')
     plt.ylabel('Modulo [dB]')
     plt.xlim([20,20000])
@@ -283,12 +285,12 @@ def plot_compare(resultsLMS, resultsRLS, P, S, xgen, soundgen):
     n = np.arange(0, len(e_lms))
     x = xgen(n)
     sound = soundgen(n)
-    display('Señal error LMS:', Audio(e_lms, rate=48000, normalize=False))
-    display('Señal error RLS:', Audio(e_rls, rate=48000, normalize=False))
+    display('Señal error LMS:', Audio(e_lms, rate=48000))
+    display('Señal error RLS:', Audio(e_rls, rate=48000))
     display('Señal interferencia:', Audio(
-        x, rate=48000, normalize=False))
+        x, rate=48000))
     display('Señal interferencia + sonido:',
-            Audio((x + sound), rate=48000, normalize=False))
+            Audio((x + sound), rate=48000))
 
 
 def createRoom(print):
